260952122881510e1da5c43e6b890115076c9d56
Prateek Gaurav <prateekgaurav23@gmail.com>
2016-02-02 18:49:00 +0530

    Create hello.c

---
+++ Pset1/hello.c
@@ -0,0 +1,13 @@
+/*
+    hello.c
+    
+    Author: Prateek Gaurav, prateekgaurav23@gmail.com
+    
+    This file is a solution of the hello world problem from pset1 of CS50X by HarvardX on edX..
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
