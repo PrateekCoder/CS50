549e6b991105997aabe1a73ed741d245a9c03932
Prateek Gaurav <prateekgaurav23@gmail.com>
2016-02-02 18:52:00 +0530

    Update vigenere.c

--- Pset2/vigenere.c
+++ Pset2/vigenere.c
@@ -1,35 +1,85 @@
 /*
-    initials.c
+    vigenere.c
    
     Author: Prateek Gaurav, prateekgaurav23@gmail.com
     
-    This file is a solution of the initials problem from pset2 of CS50X by HarvardX on edX..
+    This file is a solution of the vigenere problem from pset2 of CS50X by HarvardX on edX..
 */
 
-#include<stdio.h>
-#include<cs50.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>     //to use the function strlen().
+#include <ctype.h>      //to use is functions like islower() etc.
+#include<stdlib.h>      //to use the function atoi(), though it is included in cs50 lib.
 
-int main(void)
+int main (int argc, string argv[])
 {
-   
-   int i=0;
-   //printf("Enter a string:");
-   string s=GetString();
-   printf("%c", toupper(*s));
-   while(s[i]!='\0')
-     {
-        if(s[i]==' ')
-         {
-           i++;
-            printf("%c", toupper(*(s+i)));
-             
-         }
-         i++;
+    if (argc != 2)     //checks if the input by user is more than one, if it is more than 1 the program termiinates returning 1.
+    {
+        printf("\n");
+        return 1;
+    }
+    else if(argv[1])
+    {
+       int a=strlen(argv[1]);
+       for(int i=0; i<a; i++)
+       {
+          if(!isalpha(argv[1][i]))   //checks if the input by the user is a string.
+          {
+            printf("Please enter only alphabets.\n");
+            return 1;
+          }
+       }
+        string s = GetString();   //takes the string input from the user.
         
-     }
-     printf("\n");
-     return 0;
-   
-} 
+       
+        
+           int i = 0;
+           int m = 0;
+           int n = 0;
+           int z = 0;      //this is the keyindex.
+           string k = argv[1];
+        
+               for (i = 0, n = strlen(s); i < n; i++)
+                   { 
+                          if (isalpha(s[i]))
+                            {
+                                z = m % strlen(k);
+                                   if (islower(s[i]))
+                                      {
+                                           if (islower(k[z]))    //converts the lower case alphabets into ciphers.
+                                                 {
+                                                       int a = ((s[i] - 97 + k[z] - 97) % 26) + 97;
+                                                        printf("%c", a);
+                                                  }
+                
+                                           else
+                                                  {
+                                                       int a = ((s[i] - 97 + k[z] - 65) % 26) + 97;
+                                                       printf("%c", a);
+                                                  }
+                                      }
+                                           if (isupper(s[i]))        //converts the upper case alphabets into ciphers.
+                                                 {
+                                                         if (islower(k[z]))
+                                                            {
+                                                                  int b = ((s[i] - 65 + k[z] - 97) % 26) + 65;
+                                                                   printf("%c", b);
+                                                             }
+                                           else
+                                               {
+                                                    int b = ((s[i] - 65 + k[z] - 65) % 26) + 65;
+                                                    printf("%c", b);
+                                                }
+                                       }
+                                         m++;
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
+    }
+    printf("\n");
+}
+       
+}
