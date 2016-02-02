201ee7b014959ce67f4bf2b2b7d0cb2ba8e3c8c3
Prateek Gaurav <prateekgaurav23@gmail.com>
2016-02-02 18:49:21 +0530

    Create mario.c

---
+++ Pset1/mario.c
@@ -0,0 +1,35 @@
+/*
+    mario.c
+   
+    Author: Prateek Gaurav, prateekgaurav23@gmail.com
+    
+    This file is a solution of the mario problem from pset1 of CS50X by HarvardX on edX..
+*/
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void)
+{
+ int n,i,j,k;
+ do
+ {
+     printf("Enter the height of the pyramid:");
+     n=GetInt();
+ }
+ while(n<0||n>23);
+ for(i=0; i<n; i++)
+ {
+    for(j=(n-2); j>=i; j--)
+    {
+        printf(" ");
+        
+        
+    }
+    for(k=-2; k<i; k++)
+    {
+       printf("#");
+    }
+   
+    printf("\n");
+ }
+}
